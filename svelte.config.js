b2197f1cfacb7eb19e3b33eb7052b4a5e8472b1d
vivanov <brightsdayss@gmail.com>
2023-11-11 21:25:00 +0400

    fix address

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-  base: '/mobile-cairn/',
+  base: '/pocket-cairn/',
   plugins: [svelte()],
   preprocess: vitePreprocess(),
 })
